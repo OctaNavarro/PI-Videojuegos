84a287a8a08c9b3548faaac315fc837c5f394849
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-01-10 16:40:24 -0300

    Update index.js

--- PI-Videogames-main/client/src/actions/index.js
+++ PI-Videogames-main/client/src/actions/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-import dotenv from "dotenv"
-dotenv.config()
+// import dotenv from "dotenv"
+// dotenv.config()
 
-axios.defaults.baseURL = 'pi-videojuegos-production.up.railway.app' || process.env.URL_BACK || "http://localhost:3001/"
+//axios.defaults.baseURL = 'pi-videojuegos-production.up.railway.app' || process.env.URL_BACK || "http://localhost:3001/"
+axios.defaults.baseURL = 'pi-videojuegos-production.up.railway.app' || "http://localhost:3001/"
 
 export function getVideogames() {
   return async function (dispatch) {
