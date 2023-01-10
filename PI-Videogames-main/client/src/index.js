e96a2c2c11c70e7923fff9026094e1cbd368e737
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-01-10 12:57:11 -0300

    Update index.js

--- PI-Videogames-main/client/src/index.js
+++ PI-Videogames-main/client/src/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './Store'
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.URL_BACK || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
