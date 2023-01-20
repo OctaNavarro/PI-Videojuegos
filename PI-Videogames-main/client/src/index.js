67d7be828a084f2506232799d0fd76d2491bf62e
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-01-20 15:32:34 -0300

    Update index.js

--- PI-Videogames-main/client/src/index.js
+++ PI-Videogames-main/client/src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './Store'
+import axios from 'axios'
 // import dotenv from "dotenv"
 
 // dotenv.config()
