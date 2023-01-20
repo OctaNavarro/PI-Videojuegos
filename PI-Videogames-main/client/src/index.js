74d5f5820068703f8a0575271627b7e143aaf8d0
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-01-20 15:10:00 -0300

    Update index.js

--- PI-Videogames-main/client/src/index.js
+++ PI-Videogames-main/client/src/index.js
@@ -5,7 +5,6 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './Store'
-import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <Provider store={store}>
